81a0265359a75e9def264b5256a5a4ea225b3d8e
shathipaul <shathipaul11@gmail.com>
2022-09-03 03:44:09 +0600

    third commit

--- js/app.js
+++ js/app.js
@@ -9,10 +9,6 @@ displayCategory(data.data.news_category);
 const displayCategory = categories =>{
     // console.log(categories)
     const categoryContainer = document.getElementById('category-div');
-
-    // const categoryDiv = document.createElement('ul');
-    // categoryDiv.classList.add("list-unstyled", "d-flex", "justify-content-between", "g-3")
-    
         categories.forEach(category => {
         // console.log(category)
         const categoryDiv = document.createElement('li');
@@ -23,3 +19,62 @@ const displayCategory = categories =>{
     });
 }
 loadCategory()
+
+
+    const loadCard = async () =>{
+        const url = `https://openapi.programming-hero.com/api/news/category/01`
+        const res = await fetch(url);
+        const data = await res.json();
+        displayCard(data.data);
+        }
+
+        
+
+    const displayCard = cards =>{
+        // console.log(cards)
+        const cardContainar = document.getElementById('card-info');
+        cards.forEach(card => {
+            console.log(card)
+            const cardDiv = document.createElement('div')
+            cardDiv.innerHTML =`
+            <div class="card mb-3">
+                    <div class="row g-0">
+                      <div class="col-md-3 p-3">
+                        <img src="${card.thumbnail_url}" alt="...">
+                      </div>
+                      <div class="col-md-9">
+                        <div class="card-body p-3">
+                          <h5 class="card-title p-3">${card.title}</h5>
+                          <p class="card-text">${card.details.slice(0, 500)}</p>
+                          <!-- Card Footer start -->
+                          <div class="d-flex justify-content-between pt-4">
+                            <div class="d-flex align-items-center">
+                                <div>
+                                <img src="${card.author.img}" class="rounded-circle img-fluid" style="width: 3rem;" alt="...">
+                                </div>
+                                <div>
+                                    <p class="pt-4 ps-3">${card.author.name}</p>
+                                </div>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <div>
+                                    <i class="fa-solid fa-eye p-2"></i>
+                                </div>
+                                <div>
+                                <h6 class=" pt-2">${card.total_view}</h6>
+                                </div>
+                            </div>
+                            <div class="text-end pt-3">
+                                <button type="button" class="btn btn-primary">Primary</button>
+                            </div>
+                          </div>
+                          <!-- Card Footer End -->
+                        </div>
+                      </div>
+                    </div>
+            </div>
+            `;
+            cardContainar.appendChild(cardDiv)
+        });
+    }
+    loadCard()
